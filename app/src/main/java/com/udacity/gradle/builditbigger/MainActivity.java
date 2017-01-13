7ac7f4bbd2cdfb2c678e3636038d1c95d2a04596
Prashant Panwar <prashant.panwar777@gmail.com>
2017-01-13 12:32:34 +0530

    feat: Deploy the backend live to App Engine

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -52,6 +52,7 @@ public class MainActivity extends ActionBarActivity {
         //Joker myJoker = new Joker();
         //Toast.makeText(this, myJoker.getJoke(), Toast.LENGTH_LONG).show();
         new EndpointsAsyncTask().execute(this);
+
     }
 
     static class EndpointsAsyncTask extends AsyncTask<Context, Void, String> {
@@ -66,7 +67,7 @@ public class MainActivity extends ActionBarActivity {
                         // options for running against local devappserver
                         // - 10.0.2.2 is localhost's IP address in Android emulator
                         // - turn off compression when running against local devappserver
-                        .setRootUrl("http://192.168.1.3:8080/_ah/api/")
+                        .setRootUrl("http://106.51.234.211:8080/_ah/api/")
                         .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
                             @Override
                             public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
@@ -94,4 +95,5 @@ public class MainActivity extends ActionBarActivity {
             context.startActivity(intent);
         }
     }
+
 }
