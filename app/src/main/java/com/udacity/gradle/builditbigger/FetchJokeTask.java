e3771183ce0c269c4de28c83f00eda74b0752bda
Prashant Panwar <prashant.panwar777@gmail.com>
2017-01-17 02:49:23 +0530

    refactor: add FetchJokeTask

---
+++ app/src/main/java/com/udacity/gradle/builditbigger/FetchJokeTask.java
@@ -0,0 +1,67 @@
+package com.udacity.gradle.builditbigger;
+
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.LauncherApps;
+import android.os.AsyncTask;
+
+import com.example.prashant.myapplication.backend.myApi.MyApi;
+import com.example.prashant.mylibrary.JokeActivity;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+
+import java.io.IOException;
+
+class FetchJokeTask extends AsyncTask<Void, Void, String> {
+    private static MyApi myApiService = null;
+    private Context context;
+
+    private Callback callback;
+
+    public interface Callback{
+        void onFinished(String result);
+    }
+
+    public FetchJokeTask(Callback callback){
+        this.callback = callback;
+    }
+
+    @Override
+    protected String doInBackground(Void... params) {
+        if(myApiService == null) {  // Only do this once
+               /* MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
+                        new AndroidJsonFactory(), null)
+                        // options for running against local devappserver
+                        // - 10.0.2.2 is localhost's IP address in Android emulator
+                        // - turn off compression when running against local devappserver
+                        .setRootUrl("http://192.168.0.9:8080/_ah/api/")
+                        .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
+                            @Override
+                            public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
+                                abstractGoogleClientRequest.setDisableGZipContent(true);
+                            }
+                        });*/
+            // end options for devappserver
+
+            MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
+                    new AndroidJsonFactory(), null)
+                    .setRootUrl("https://build-it-bigger-155422.appspot.com/_ah/api/");
+
+            myApiService = builder.build();
+        }
+
+        try {
+            return myApiService.tellJoke().execute().getData();
+        } catch (IOException e) {
+            return e.getMessage();
+        }
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        if(result != null){
+            callback.onFinished(result);
+        }
+    }
+}
--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,22 +1,14 @@
 package com.udacity.gradle.builditbigger;
 
-import android.content.Context;
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-
-import com.example.prashant.myapplication.backend.myApi.MyApi;
 import com.example.prashant.mylibrary.JokeActivity;
-import com.google.api.client.extensions.android.http.AndroidHttp;
-import com.google.api.client.extensions.android.json.AndroidJsonFactory;
 
-import java.io.IOException;
-
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements FetchJokeTask.Callback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,52 +38,15 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void tellJoke(View view) {
-        new EndpointsAsyncTask().execute(this);
+    public void tellJoke(View view){
+        new FetchJokeTask(this).execute();
+
     }
 
-    static class EndpointsAsyncTask extends AsyncTask<Context, Void, String> {
-        private static MyApi myApiService = null;
-        private Context context;
-
-        @Override
-        protected String doInBackground(Context... params) {
-            if(myApiService == null) {  // Only do this once
-               /* MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
-                        new AndroidJsonFactory(), null)
-                        // options for running against local devappserver
-                        // - 10.0.2.2 is localhost's IP address in Android emulator
-                        // - turn off compression when running against local devappserver
-                        .setRootUrl("http://192.168.0.9:8080/_ah/api/")
-                        .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
-                            @Override
-                            public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
-                                abstractGoogleClientRequest.setDisableGZipContent(true);
-                            }
-                        });*/
-                // end options for devappserver
-
-                MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
-                        new AndroidJsonFactory(), null)
-                        .setRootUrl("https://build-it-bigger-155422.appspot.com/_ah/api/");
-
-                myApiService = builder.build();
-            }
-
-            context = params[0];
-
-            try {
-                return myApiService.tellJoke().execute().getData();
-            } catch (IOException e) {
-                return e.getMessage();
-            }
-        }
-
-        @Override
-        protected void onPostExecute(String result) {
-            Intent intent = new Intent(context, JokeActivity.class);
-            intent.putExtra(JokeActivity.JOKE_KEY, result);
-            context.startActivity(intent);
-        }
+    @Override
+    public void onFinished(String result) {
+        Intent intent = new Intent(this, JokeActivity.class);
+        intent.putExtra(JokeActivity.JOKE_KEY, result);
+        startActivity(intent);
     }
 }
