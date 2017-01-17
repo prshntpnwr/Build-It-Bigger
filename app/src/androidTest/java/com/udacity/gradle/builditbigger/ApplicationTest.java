b95aae4561a32232e26ed8c07f0d7693efa0fe99
Prashant Panwar <prashant.panwar777@gmail.com>
2017-01-17 21:20:28 +0530

    feat: Add Functional Tests

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/ApplicationTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/ApplicationTest.java
@@ -2,6 +2,7 @@ package com.udacity.gradle.builditbigger;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
+import android.util.Log;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
@@ -9,5 +10,19 @@ import android.test.ApplicationTestCase;
 public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
         super(Application.class);
+    }
+
+    public void FetchJokeTaskTest() throws Exception {
+        FetchJokeTask jokeTask = new FetchJokeTask(new FetchJokeTask.Callback() {
+            @Override
+            public void onFinished(String result) {
+                //do nothing
+            }
+
+        });
+        jokeTask.execute();
+        String jokestr = jokeTask.get();
+        Log.d("Test: ", "FetchJokeTaskTest : " + jokestr);
+        assertNotNull(jokestr);
     }
 }
